a8e1afb44e5b17a3195bd9f2ed408aa1b281c103
swyssauro <xspynni23@gmail.com>
2020-07-26 13:33:43 -0300

    add cors in main

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const cors = require('@koa/cors');
 const applyRoutes = require('./routes');
 const bodyParser = require('koa-bodyparser');
 
@@ -9,7 +10,7 @@ const app = new Koa();
 module.exports = () => {
     
     applyRoutes(router)
-    app.use(bodyParser()).use(router.routes()).use(router.allowedMethods())
+    app.use(bodyParser()).use(router.routes()).use(cors()).use(router.allowedMethods())
     
     app.listen(8080, () => console.log('online port.'));
 }
